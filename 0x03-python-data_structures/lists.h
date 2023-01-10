5dd5de550dfcb4b63b0bb272e441b6a8790f7b5a
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-01-10 08:47:14 -0800

    Created header

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,18 +1,27 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-def print_list_integer(my_list=[]):
-def element_at(my_list, idx):
-def replace_in_list(my_list, idx, element):
-def print_reversed_list_integer(my_list=[]):
-def new_in_list(my_list, idx, element):
-def no_c(my_string):
-def print_matrix_integer(matrix=[[]]):
-def add_tuple(tuple_a=(), tuple_b=()):
-def multiple_returns(sentence):
-def max_integer(my_list=[]):
-def divisible_by_2(my_list=[]):
-def delete_at(my_list=[], idx=0):
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - Singly linked list
+ * @n: interger
+ * @next: pointer to the next node
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+
+{
+       	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint_t(listint_t *head);
+int check_pal(listint_t **head, listint_t *last);
 int is_palindrome(listint_t **head);
 
-#endif
+#endif /*LISTS_H*/ 
