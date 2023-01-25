98d0554c1f94ed2d91e8a8fab937105f64811367
OfentseLoeto <ohentseloeto610@gmail.com>
2023-01-25 00:31:01 -0800

    prints the first elements of a list

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -4,21 +4,21 @@ safe_print_list_integers = __import__('2-safe_print_list_integers').safe_print_l
 
 
 
-my_list = [1, 2, 3, 4, 5]
+num = [1, 2, 3, 4, 5]
 
 
 
-nb_print = safe_print_list_integers(my_list, 2)
+nb_print = safe_print_list_integers(num, 2)
 
 print("nb_print: {:d}".format(nb_print))
 
 
 
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+num = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
 
-nb_print = safe_print_list_integers(my_list, len(my_list))
+nb_print = safe_print_list_integers(num, len(num))
 print("nb_print: {:d}".format(nb_print))
 
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+nb_print = safe_print_list_integers(num, len(num) + 2)
 
 print("nb_print: {:d}".format(nb_print))
