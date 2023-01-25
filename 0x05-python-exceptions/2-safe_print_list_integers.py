b85c7e7fefca8504cfd46dbc977bb390b233aaf2
OfentseLoeto <ohentseloeto610@gmail.com>
2023-01-25 02:56:43 -0800

    Print first element of the list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
         num = my_list[0]
 
 
-        print("{:d}".format(my_list), end="\n")
+        print("{:d\n}".format(my_list), end=",")
 
     except (ValueError, TypeError):
         print("")
