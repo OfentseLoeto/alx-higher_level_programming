a1ea0d3c8a0e54f79896d25018df1a139d0b9b92
OfentseLoeto <ohentseloeto610@gmail.com>
2023-01-28 02:30:15 -0800

    Task created

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+def islower(c):
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):
+def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
+
+
+#endif /*LISTS_H*\
---
+++ 0x01-python-if_else_loops_functions/lists2.h
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+def islower(c):
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
