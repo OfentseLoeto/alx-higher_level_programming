57f5d72b5c9416ca10a98e1c2ea7eb3f54a9af79
OfentseLoeto <ohentseloeto610@gmail.com>
2023-01-31 14:16:49 -0800

    string removed

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -24,13 +24,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 
-/*
+
 def islower(c) :
 def uppercase(str) :
 def print_last_digit(number) :
 def add(a, b) :
 def pow(a, b) :
 def fizzbuzz(void) :
-*/
+def remove_char_at(str, n):
 
 #endif /* LISTS_H */
