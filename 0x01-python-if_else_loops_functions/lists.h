13d109473abf101c243a101e509fa69a26a81a76
OfentseLoeto <ohentseloeto610@gmail.com>
2023-01-31 13:34:49 -0800

    function in C that inserts a number into a sorted singly linked lis

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -2,16 +2,35 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+/**
+ * struct listint_s - Singly liked list
+ * @n: Integer
+ * @next: Pointer to the next node
+ *
+ * Description: Singly linked list structure
+ */
 
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
 
-def islower(c):
-def uppercase(str):
-def print_last_digit(number):
-def add(a, b):
-def pow(a, b):
-def fizzbuzz():
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 
+/*
+def islower(c) :
+def uppercase(str) :
+def print_last_digit(number) :
+def add(a, b) :
+def pow(a, b) :
+def fizzbuzz(void) :
+*/
 
-#endif /*LISTS_H*\
+#endif /* LISTS_H */
