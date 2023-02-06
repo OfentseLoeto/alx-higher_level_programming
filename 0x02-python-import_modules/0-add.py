523e72f9f23de97c0929ac4344b1ad1312f5a1f2
OfentseLoeto <ohentseloeto610@gmail.com>
2023-02-06 01:38:59 -0800

    add two integers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-def add(a, b):
-
     if __name__ == "__main__":
 
         from add_0 import add
@@ -9,4 +7,4 @@ def add(a, b):
         a = 1
         b = 2
 
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        print("{} + {} = {}".format(a, b, add(a, b)))
