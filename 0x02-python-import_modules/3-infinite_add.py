765933b66c942b3cd3ede499253fcd533cb88e51
OfentseLoeto <ohentseloeto610@gmail.com>
2023-02-06 07:13:59 -0800

    prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
 
     from sys import argv
 
-    num = 0
+    numb = 0
     for index in argv[1:]:
-        num += int(index)
-        print('{:d}'.format(num))
+        numb += int(index)
+        print('{:d}'.format(numb))
