e777ca434fa09ac5dd9ab70fb1338b2bc46d1145
OfentseLoeto <ohentseloeto610@gmail.com>
2023-02-06 03:06:42 -0800

    Task created

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+def add(a, b):
+
+    """My addition function
+
+    Args:
+
+    a: 10
+    b: 5
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+
+    """My subtraction function
+    Args:
+
+    a = 10
+    b = 5
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+
+    """My multiplication funtion
+
+    Args:
+
+    a = 10
+    b = 5
+
+    Returns:
+        The return value. a * b
+    """
+
+    return (a * b)
+
+def div(a, b):
+
+    """My division function
+
+    Args:
+
+    a = 10
+    b = 5
+
+    Returns:
+        The return value. a / b
+    """
+
+    return (a / b)
