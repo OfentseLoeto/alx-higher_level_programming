32c7f050c72823f7ce2aeef17030e802780056de
OfentseLoeto <ohentseloeto610@gmail.com>
2023-02-09 11:25:02 -0800

    Task created

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,4 @@
 
 def square_matrix_simple(matrix=[]):
 
-    new_matrix = []
-    for i in matrix:
-        new_matrix[len(new_matrix):] = [[elem ** 2 for elem in i]]
-
-        return new_matrix
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
