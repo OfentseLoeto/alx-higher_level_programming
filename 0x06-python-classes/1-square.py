26f9423f532134f9f5deac141aff502257ffbca8
OfentseLoeto <ohentseloeto610@gmail.com>
2023-02-22 10:51:31 -0800

    Define a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
+"""Define a class square."""
+
 class Square:
+"""Represent a square."""
 
     def __init__(self, size):
+        """Initialize anew square.
 
         Args:
+        size (int): The size of anew square.
+        """
 
-            size (int):
-                self.__size = size
+        self.__size = size
