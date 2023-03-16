83d5e94220ddf8dd66550b7cc6bf069e5fb85b48
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-03-16 13:03:06 -0700

    computes the score average of all records

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,4 @@
--- computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT AVG(score) AS avarage FROM second_table;
+-- computes the score average of all records in the 
+-- table second_table of the  MySQL server.
+SELECT AVG(score) AS 'avarage'
+FROM second_table;
