3b3c38e1e9b349eccf485d351ef352e96739aaef
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-03-16 13:19:30 -0700

    lists the number of records with the same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- lists the number of records with the same score
 SELECT score;
-COUNT (*) AS "number"
+COUNT(score) AS number
 FROM second_table
 GROUP BY score
-ORDER BY "number" DESC;
+ORDER BY score DESC;
