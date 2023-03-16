c97b0646779b82c30b4a2e32f53da2ef041d68c1
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-03-16 14:07:26 -0700

    lists the number of records with the same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
 -- lists the number of records with the same score
-SELECT score, name
+SELECT score, COUNT(score)
 FROM second_table
-GROUP BY score
+GROUP BY score AS number
+HAVING COUNT(score) >1
 ORDER BY score DESC;
