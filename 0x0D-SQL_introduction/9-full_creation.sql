846c832559032faf7467fc2a8fb5dd1504d6c5f9
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-03-17 19:56:08 -0700

    created a table with multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,8 @@
 -- creates a table second_table in the database hbtn_0c_0
 -- in your MySQL server and add multiples rows.
 
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.student_pfmc.second_table (
-	pk_username VARCHAR PRINARY KEY, id INT, name VARCHAR(256), score INT) 
-	VALUES ('pk_Ofentse', 1, 'John', 10),
-	       ('pk_Ofentse', 2, 'Alex', 3),
-	       ('pk_Ofentse', 3, 'Bob', 14),
-	       ('pk_Ofentse', 4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) values (1, 'John', 10);
+INSERT INTO second_table (id, name, score) values (2, 'Alex', 3);
+INSERT INTO second_tabel (id, name, score) values (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) values (4, 'George', 8);
