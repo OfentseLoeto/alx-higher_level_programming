0b01c3e7947b115dd2a3c98a6f45b0eaec2383c5
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-03-17 18:50:16 -0700

    Get average of all records in the table

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,5 +1,5 @@
 -- computes the score average of all records in the 
 -- table second_table of the  MySQL server.
 
-SELECT AVG(score) AS [AVG avarage]
-FROM second_table;
+SELECT AVG(score) AS [avarage]
+FROM [hbtn_0c_0].[second_table]
