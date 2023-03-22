fc4ad55a1345d6d03a16bd0cf93f5ba8eb4855af
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-03-22 04:53:06 -0700

    Created a database and the user_accoun

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,9 +3,9 @@
 --The user_0d_2 password should be set to user_0d_2_pwd
 -- If the database hbtn_0d_2 already exists, your script should not fail
 
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_02`;
-CREATE USER IF NOT EXISTS 'user_0d_02'@'localhost'
-IDENTIFIED BY 'user_0d_02_pwd';
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
 
 GRANT SELECT 
 ON `hbtn_0d_2`.* 
