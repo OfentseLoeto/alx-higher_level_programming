d16aceb40ea69e126b58f944265201a725f8c268
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-03-22 01:22:53 -0700

    List all privilages from users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges of the MySQL users user_0d_1
 -- and user_0d_2 on a server (in localhost).
 
-SHOW GRANTS user_0d_1@localhost, user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost, user_0d_2@localhost;
