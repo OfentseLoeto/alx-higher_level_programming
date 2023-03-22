ded6e223ef5c2310da7af740d041d33b827aa358
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-03-22 07:05:02 -0700

    Created tables with primary and foreign keys which references each other

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -12,5 +12,5 @@ create table hbtn_0d_usa.cities(
 	id int not null auto_increment,
 	state_id int not null,
 	name varchar(256) not null,
-	FOREIGN KEY(state_id),
-	REFERENCES `hbtn_0d_usa`.`state`(id);
+	FOREIGN KEY(state_id)
+	REFERENCES `hbtn_0d_usa`.`states`(id);
