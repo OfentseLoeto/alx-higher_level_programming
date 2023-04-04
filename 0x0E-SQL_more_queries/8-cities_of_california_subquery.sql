f411cdf47b47ec05dea6b8e085cda08e47c6cb6f
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-04-04 10:59:26 -0700

    Selecting all data from a table and sort by ascending order

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,7 @@
 -- lists all the cities of California
 -- Results must be sorted in ascending order by cities.id
 
-SELECT * FROM `cities`
-WHERE `state_id` = (SELECT `id` 
-	FROM `states` 
-	WHERE `name` = `California`
-ORDER BY  `id` ASC);
+SELECT id, name, cities.id
+FROM states
+WHERE name = California
+ORDER BY cities.id;
