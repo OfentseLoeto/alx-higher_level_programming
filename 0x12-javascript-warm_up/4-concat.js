2ed9dd9b2935dd73f05d0e6a9a6ac01d723f38ae
OfentseLoeto <ofentseloeto610@gmail.com>
2023-04-13 12:54:54 -0700

    concatinate strings

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,5 +2,5 @@
 
 /* script that prints two arguments passed to it */
 
-const [arg1, arg2] = process.argv.slice(2);
-console.log(`${arg1} is ${arg2}`);
+const [arg1, arg2] = process.argv.slice(2)
+console.log(`${arg1} is ${arg2}`)
