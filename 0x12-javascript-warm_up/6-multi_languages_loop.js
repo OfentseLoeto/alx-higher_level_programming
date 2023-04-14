73d768c453d800a395a0bae248b9fb60d71e07de
OfentseLoeto <ofentseloeto610@gmail.com>
2023-04-14 00:51:21 -0700

    Create an array of string

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 
 // script that prints 3 lines
-//  but by using an array of string and a loop
+// but by using an array of string and a loop
+
+const lan = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lan.length; i++) {
+  console.log(lan[i]);
+}
