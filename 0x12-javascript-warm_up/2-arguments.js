48137eb2237d8ce73e1a98f310a589b655e6626e
OfentseLoeto <ofentseloeto610@gmail.com>
2023-04-14 00:03:45 -0700

    script that prints 3 lines

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 
-const argc = process.argv.length
+//  prints a message depending of the number of arguments passed
+//  If no arguments are passed to the script, print “No argument"
+//  If only one argument is passed to the script, print “Argument found”
+//  Otherwise, print “Arguments found”
+
+const argc = process.argv.length;
 if (argc === 3) {
-	console.log('Argument found')
-
+  console.log('Argument found');
 } else if (argc === 2) {
-	console.log('No argument')
-
+  console.log('No argument');
 } else {
-	console.log('Arguments found')
+  console.log('Arguments found');
 }
