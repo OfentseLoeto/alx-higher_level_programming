84b227d74fdc1250734469fa0a3c38c25a32c3ca
OfentseLoeto <ofentseloeto610@gmail.com>
2023-04-14 08:08:39 -0700

    script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+// script that prints a square
+// The first argument is the size of the square
+// If the first argument can’t be converted to an integer, print “Missing size”
+// use the character X to print the square
+
+const arg = process.argv[2];
+
+// convert arg to int
+const size = parseInt(arg);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'x';
+    }
+    console.log(row);
+  }
+}
