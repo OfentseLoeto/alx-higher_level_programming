22f1ff74e35134bb197eb05abd092eb137eb4f96
OfentseLoeto <ofentseloeto610@gmail.com>
2023-04-15 14:37:04 -0700

    class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,12 @@
 
 // Write a class Rectangle that defines a rectangle
 
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if ((w > 0) && (h > 0)) {
-    this.width = w;
-    this.height = h;
+      this.width = w;
+      this.height = h;
+    }
   }
-  }
-};
+}
+module.exports = Rectangle;
