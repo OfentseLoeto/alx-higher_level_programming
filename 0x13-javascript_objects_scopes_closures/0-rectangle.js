b9fd60f50585d159e98052223f70d0c62b3a3ee0
OfentseLoeto <ofentseloeto610@gmail.com>
2023-04-15 11:01:17 -0700

    Created an empty class

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -3,5 +3,5 @@
 //  an empty class Rectangle that defines a rectangle
 //  use the class notation for defining your class
 
-module.exports = class Ractangle {
+module.exports = class Rectangle {
 };
