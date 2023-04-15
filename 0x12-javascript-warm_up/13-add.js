9fcc0934b554558d2de30125b06378c064c53a1e
OfentseLoeto <ofentseloeto610@gmail.com>
2023-04-15 00:17:03 -0700

    function that returns the addition of 2 integers.

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// function that returns the addition of 2 integers.
+// The function must be visible from outside
+// The name of the function must be add
+
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(4, 8));
