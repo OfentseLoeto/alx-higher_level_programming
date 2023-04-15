dc270e7c82ca70a426db8e1f0e61a50778060fd0
OfentseLoeto <ofentseloeto610@gmail.com>
2023-04-15 04:30:55 -0700

    script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -7,14 +7,32 @@
 // You must do it recursively
 // You must use a function
 
-function factorial (n) {
-  if (isNaN(n)) {
-    return 1;
-  } else if (n === 0 || n === 1) {
+//function factorial (n) {
+  //if (isNaN(n)) {
+    //return 1;
+//  } else if (n === 0 || n === 1) {
+  //  return 1;
+ // } else {
+   // return n * factorial(n - 1);
+  //}
+//}
+//const n = parseInt(process.argv[2]);
+//console.log(`The factorial of ${n} is ${factorial(n)}.`);
+
+function factorial (a) {
+  if (a <= 0) {
+    return 0;
+  } else if (a === 1) {
     return 1;
   } else {
-    return n * factorial(n - 1);
+    return (a * factorial(a - 1));
   }
 }
-const n = parseInt(process.argv[2]);
-console.log(`The factorial of ${n} is ${factorial(n)}.`);
+
+const myInt = parseInt(process.argv[2]);
+if (isNaN(myInt)) {
+  console.log('1');
+} else {
+  console.log(factorial(myInt));
+}
+
