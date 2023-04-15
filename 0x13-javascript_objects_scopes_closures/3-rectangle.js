ccdd6dc6b6e1adfabdfaeff719cf0e115874bf68
OfentseLoeto <ofentseloeto610@gmail.com>
2023-04-15 13:53:59 -0700

    Createdclass Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,7 +4,7 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 && isNaN(w, h)) return;
+    if (w <= 0 || h <= 0) return '';
     this.width = w;
     this.height = h;
   }
