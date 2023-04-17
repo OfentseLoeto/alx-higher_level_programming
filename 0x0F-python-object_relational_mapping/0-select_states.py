295560ce660965217682969e5a733843e30498ed
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-04-17 14:49:59 -0700

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    row = cur.fetchall()
+    rows = cur.fetchall()
     
     for row in rows:
         print(row)
