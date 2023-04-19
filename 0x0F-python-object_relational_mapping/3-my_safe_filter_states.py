4fff7c0fb6bdbce44987307e3b46ca4493f016d2
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-04-19 04:48:44 -0700

    script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
-
+"""
+Once again, write a script that takes in arguments
+and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that
+is safe from MySQL injections!
+"""
 import sys
 import MySQLdb
 
+
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    search = sys.argv[4]
+    cur.execute("""SELECT id,name FROM states where name = %s
+                ORDER by id ASC""", (search,))
+    row = cur.fetchall()
+    for rw in row:
+        print(rw)
+    cur.close()
+    conn.close()
+
+
 if __name__ == "__main__":
-
-    if len (sys.argv) != 5:
-        print("Usage: {} username password database_name states_name".format(sys.argv[0]))
-        exit(1)
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database_name = sys.argv[3]
-        states_name = sys.argv[4]
-
-        conn = MySQLdb.connect(
-                host="hostname",
-                port=3306,
-                user=user_name,
-                passwd=password,
-                db=database_name)
-
-        cur = conn.cursor()
-        cursor.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (states_name))
-
-        row = cur.fetchall()
-        for row in rows:
-            print(row)
-
-            cur.close()
-            conn.close()
+    main()
