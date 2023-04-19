3f63a2329e0c882d1b9cef3e46af8673e842057a
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-04-19 04:56:25 -0700

    script that takes in an argument and displays all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
-
-""" script that takes in an argument and displays 
-all values in the states table of hbtn_0e_0_usa 
+"""
+Write a script that takes in an argument and
+displays all values in the states table of hbtn_0e_0_usa
 where name matches the argument.
 """
-
 import sys
 import MySQLdb
 
+
 def main():
-
     conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-            )
-
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
     cur = conn.cursor()
     search = sys.argv[4]
-
-    query = """SELECT * FROM states WHERE name = '{:s}' ORDER BY id ASC""".format(search)
-
+    query = """SELECT * FROM states where name = '{:s}'
+            ORDER by id ASC""".format(search)
     cur.execute(query)
+    row = cur.fetchall()
+    for rw in row:
+        if rw[1] == search:
+            print(rw)
+    cur.close()
+    conn.close()
 
-    rows = cur.fetchall()
 
-    for row in rows:
-        if row[1] == search:
-            print(row)
-
-
-        cur.close()
-        conn.close()
-
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
