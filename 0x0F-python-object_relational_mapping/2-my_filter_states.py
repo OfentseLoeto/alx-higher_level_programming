8bc4b818dbb14e773445376d49f638c1f86329e8
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-04-19 04:10:19 -0700

    script that takes in an argument and displays all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
 
-""" script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument."""
+""" script that takes in an argument and displays 
+all values in the states table of hbtn_0e_0_usa 
+where name matches the argument.
+"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == '__main__':
+def main():
 
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=database, state_name=sys.argv[4], port=3306)
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
 
-    cur = db.cursor()
+    cur = conn.cursor()
+    search = sys.argv[4]
 
-    query = ("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC;".format(sys.argv[4]))
+    query = """SELECT * FROM states WHERE name = '{:s}' ORDER BY id ASC""".format(search)
 
     cur.execute(query)
 
@@ -19,3 +29,9 @@ if __name__ == '__main__':
 
     for row in rows:
         print(row)
+
+        cur.close()
+        conn.close()
+
+    if __name__ == "__main__":
+        main()
