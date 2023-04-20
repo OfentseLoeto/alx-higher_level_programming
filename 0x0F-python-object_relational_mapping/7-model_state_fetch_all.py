1aefd5bf2358c73a293579970d9cfa588f306d1e
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-04-20 12:27:42 -0700

    Write a script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-"""Lists all State objects from the database hbtn_0e_6_usa"""
+"""
+Lists all State objects from the database hbtn_0e_6_usa
+"""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
 if __name__ == '__main__':
-    # Check number of arguments
-    if len(sys.argv) != 4:
-        print('Usage: {} username password database'.format(sys.argv[0]))
-        sys.exit(1)
+    # Get command line arguments
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    # Connect to database
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(username, password, database),
-                           pool_pre_ping=True)
-
-    # Create session
+    # Create engine and session
+    engine = create_engine(f'mysql://{mysql_user}:{mysql_password}@localhost:3306/{db_name}')
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query State objects and print results
-    for state in session.query(State).order_by(State.id):
-        print('{}: {}'.format(state.id, state.name))
+    # Query for all State objects and print them out
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f'{state.id}: {state.name}')
 
-    # Close session
+    # Close session and engine
     session.close()
-
+    engine.dispose()
