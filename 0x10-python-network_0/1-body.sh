669b65c384a4cfcbf2b9926f58d1d77477514e86
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-05-03 18:49:27 -0700

    Display only body of a 200 status code response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a GET request to the URL using curl in silent mode, and displays the body of the 
-#response if the response status code is 200
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-if [[ $response == "200" ]]; then
-curl -s "$1"
-fi
+# sends a GET request to the URL using curl in silent mode, and displays the body of the response
+curl -sL "$1"
