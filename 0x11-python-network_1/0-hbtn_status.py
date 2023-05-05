cdeb5b12c5a45cb70d0e4a5cdd28c122cd767526
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-05-05 13:53:56 -0700

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,5 +6,5 @@ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     data = response.read()
     print("Body response:")
     print("/t- type: {}".format(type(data)))
-    print("/t- content {}".format(data))
-    print("/t- utf8 content {}".format(data.decode('utf8')))
+    print("/t- content: {}".format(data))
+    print("/t- utf8 content: {}".format(data.decode('utf-8')))
