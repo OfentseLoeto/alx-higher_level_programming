d39ae65f07f0ef3425efe9e625eec5b98e5438f9
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-05-10 03:14:59 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-// Scrit that prints "javascript is amazing"
-
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-/* script that prints two arguments passed to it */
-
-const [arg1, arg2] = process.argv.slice(2);
-console.log(`${arg1} is ${arg2}`);
