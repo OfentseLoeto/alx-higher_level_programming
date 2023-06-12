2df04fdc69f9cf894ab6caa500dd1c1a7a2b44d2
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-12 13:37:04 -0700

    pring 3 lines using an array of string and loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// print three lines using an array of string and loop
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
