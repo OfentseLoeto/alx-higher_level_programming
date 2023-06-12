dca7dd83f5549991bcd095bd9e5471a2baf18824
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-12 11:21:00 -0700

    Print a message depending of the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/*
+ *  prints a message depending of the number of arguments passed
+ */
+
+const args = process.argv.slice(2);
+
+if (args.length === 0){
+	console.log('No Argument');
+} else if (args.length === 1){
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
