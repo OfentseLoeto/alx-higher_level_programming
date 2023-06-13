ef9f8f167692a7d400f0ff5dc1d0eb050fd05c7d
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-13 11:20:11 -0700

    Creating a class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+// A class Rectangle that defines a rectangle
+// Then check if w or h is equal to 0 or not a 
+// positive integer, create an empty object
+
+class Rectangle {
+  
+  constructor (w, h){
+    
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+
+// Returning an empty object if width or height is invalid
+      return {};
+  }
+  this.width = w;
+  this.height = h;
+}
+};
+module.exports = Rectangle;
