f28114cdc7149026f4336e95980bdd19dcff0975
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-13 02:25:58 -0700

    Replacing the value of an object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+//  replacing the value of an object
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.value = 89;
+console.log(myObject);
