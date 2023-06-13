f5aeee68518dc02d61093c955dc5bdc333b065e2
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-13 15:33:08 -0700

    Created method called print() that prints the rectangle using the character X

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,17 +4,17 @@
 // Then check if w or h is equal to 0 or not a 
 // positive integer, create an empty object
 
-class Rectangle {
-  
+const Rectangle = class {
+
   constructor (w, h){
     
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
 
 // Returning an empty object if width or height is invalid
       return {};
-  }
-  this.width = w;
-  this.height = h;
+    }
+    this.width = w;
+    this.height = h;
 }
 };
 module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+// Creating a class Rectangle that defines a rectangle
+// Then if w or h is equal to 0 or not a positive integer, create an empty object
+// And Create an instance method called print()
+// that prints the rectangle using the character X
+
+const Rectangle = class {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // Return an empty object if width or height is invalid
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    if (Object.keys(this).length === 0) {
+      console.log('Empty object');
+      return;
+    }
+    const row = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+};
+module.exports = Rectangle;
