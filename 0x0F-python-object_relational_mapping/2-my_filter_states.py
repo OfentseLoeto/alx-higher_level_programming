8ffe36cd1c783d8e9adeb15d4e01c8258feb4209
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-16 08:01:58 -0700

    displays all values in the states table of hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username=sys.argv[1]
+    password=sys.argv[2]
+    db_name=sys.argv[3]
+    state_name=sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(state_name))
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
