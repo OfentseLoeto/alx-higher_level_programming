8b73795639fc4917b64a2ee6a47bb3fc1fc5831f
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-16 05:18:44 -0700

    print all data in a table

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
+    username=sys.argv[1]
+    password=sys.argv[2]
+    db_name=sys.argv[3]
 
     # Connecting to mysql server
     db = MySQLdb.connect(host="localhost", port=3306, 
-            user=argv[1], 
-            password=argv[2], 
-            database=argv[3])
+            user=username, 
+            passwd=password, 
+            db=database)
 
-    cursor = db.cursor()
+    cur = db.cursor()
 
     # Excecuting a query
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
