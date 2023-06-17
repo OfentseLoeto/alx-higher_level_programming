0cec94cd9c631e71f78baa1046d38725700d3edc
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-17 02:54:11 -0700

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 import MySQLdb
-import sys
 
-if __name__ == "__main__":
-    username=sys.argv[1]
-    password=sys.argv[2]
-    db_name=sys.argv[3]
+def list_states(username, password, database):
 
     # Connecting to mysql server
-    hbtn_0e_0_usa = MySQLdb.connect(host="localhost", port=3306, 
-            user=username, 
-            passwd=password, 
-            db=hbtn_0e_0_usa)
+    db = MySQLdb.connect(user=username, passwd=password, db=database, host='localhost', port=3306)
+    
+    cursor = db.cursor()
 
-    cur = hbtn_0e_0_usa.cursor()
+    query = "SELECT * FROM states ORDER ORDER BY id ASC"
+    cursor.execute(query)
 
-    # Excecuting a query
-    query = "SELECT * FROM states ORDER BY id ASC"
-    query = cur.execute(query)
+    row = cursor.fetchall()
 
-    rows_query = cur.fetchall()
-    for row in rows_query:
+    for row in rows:
         print(row)
 
-    cur.close()
-    hbtn_0e_0_usa.close()
+    cursor.close()
+    db.close()
+
+ if __name__ : "__main__":
+     import sys
+
+     username=sys.argv[1]
+     password=sys.argv[2]
+     database=sys.argv[3]
+
+     list_states(username, password, database)
