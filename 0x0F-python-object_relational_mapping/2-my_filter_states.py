d48e2bc773472603e5cc52096321c935bda85d35
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-22 13:55:22 -0700

    displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,17 +4,26 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username=sys.argv[1]
-    password=sys.argv[2]
-    db_name=sys.argv[3]
-    state_name=sys.argv[4]
+    """
+    Connects to a MySQL server and executes a query to retrieve matching states.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=db_name, port=3306)
+    db = MySQLdb.connect(host='localhost', user=username, 
+            passwd=password, database=database_name, port=3306)
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC""".format(state_name))
+    query = ("""SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC""")
+    param = (state_name,)
 
-    for row in cur.fetchall():
+    cur.execute(query, param)
+
+    rows = cur.fetchall()
+
+    for row in rows:
         print(row)
 
     cur.close()
