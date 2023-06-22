38f46df5067f835562c36dc827d1a0b5ea8e4671
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-22 13:07:39 -0700

    displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-import sys
+"""displays all values in the states table of hbtn_0e_0_usa where name matches the argument."""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     username=sys.argv[1]
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     db_name=sys.argv[3]
     state_name=sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=db_name, port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC".format(state_name))
+    cur.execute("""SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC""".format(state_name))
 
     for row in cur.fetchall():
         print(row)
