6f1ed0dea1e5fe13b5b00e421064a730d5e2bc26
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-06-23 12:35:33 -0700

    Created a table

--- 0x0F-python-object_relational_mapping/6-model_states.py
+++ 0x0F-python-object_relational_mapping/6-model_states.py
@@ -2,11 +2,31 @@
 """
 Start link class to table in database 
 """
-import sys
-from model_state import Base, State
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
-from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+def create_states_table():
+    """
+    Create a connection to the database
+    """
+    engine = create_engine('mysql://username:password@localhost:3306/database')
+
+    # Create the tables
     Base.metadata.create_all(engine)
+    print("States table created successfully!")
+
+
+if __name__ == '__main__':
+    create_states_table()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,20 +1,33 @@
 #!/usr/bin/pyhton3
-
+"""
+python file that contains the class definition
+of a State and an instance Base = declarative_base()
+"""
+import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class State(Base):
+    """
+    This class defines the states table
+    """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
 def create_states_table():
+    """
+    This function creates a connection to the database
+    """
     engine = create_engine('mysql://username:password@localhost:3306/database')
 
     # Create the table
     Base.metadata.create_all(engine)
+    print("States table is created successfully")
+
+if __name__ == '__main__':
+    create_states_table()
