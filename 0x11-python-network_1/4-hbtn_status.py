55afcb4741a778969b25fdc285466e34bc8ece83
ohentse loeto <ohentseloeto610@gmail.com>
2023-07-05 11:55:16 -0700

    fetch and display the body response of a URL

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-'''
-Module to fetch and display the body response of a URL.
-'''
 import requests
+# Module to fetch and display the body response of a URL
+
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
 
     response = requests.get('https://alx-intranet.hbtn.io/status')
     body = response.text
+    # GET request to the URL and retrieve the response
 
 
     print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
+
+    # Print the body response along with the type of the content and the content itself
