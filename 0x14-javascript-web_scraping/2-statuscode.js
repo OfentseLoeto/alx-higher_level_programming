09c3e46345f5990e980bd723885dbaffc81309d1
ofentseloeto <ofentseloeto610@gmail.com>
2023-07-26 13:52:27 -0700

    display the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,19 +4,17 @@
 
 const request = require('request');
 
-function getStatusCode (url) {
-  request.get(url, (error, response) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(`Code: ${response.statusCode}`);
-    }
-  });
+if (process.argv.length !== 3) {
+  console.error('Usage: node get-statuscode.js <URL>');
+  process.exit(1);
 }
 
-if (process.argv.length !== 3) {
-  console.error('Usage: 2-statuscode.js <URL>');
-} else {
-  const url = process.argv[2];
-  getStatusCode(url);
-}
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error.message);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
