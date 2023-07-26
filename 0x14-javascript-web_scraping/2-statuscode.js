2977438ad33aec551ec76de85975379f8aeb0083
ofentse loeto <ofentseloeto610@gmail.com>
2023-07-26 06:19:00 +0000

    Send aget request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+'use strict';
+// Script that display the status code of a GET request.
+
+const request = require('request');
+
+function getStatusCode(url) {
+  request.get(url, (error, response) => {
+
+    if (error) {
+      console.error(error);
+
+} else {
+  console.log('Code: ${response.statusCode}');
+}
+});
+}
+
+if (process.argv.length !== 3) {
+ console.error('Usage: 2-statuscode.js <URL>');
+
+} else {
+  const url = process.argv[2];
+  getStatusCode(url);
+} 
