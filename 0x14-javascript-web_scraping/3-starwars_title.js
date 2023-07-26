c6d68097590d1bf4ddb983336cc94f1aa0f9733d
ofentse loeto <ofentseloeto610@gmail.com>
2023-07-26 13:39:35 +0000

    Get title by id

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,31 @@
 #!/usr/bin/node
-
-// A script that prints the title of a Star Wars movie where the episode number matches a given integer.
+// Script that prints the title of a Star Wars movie where the episode number matches a given integer.
 
 const request = require('request');
-const movieId = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/1/';
 
-request (url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-} else {
-  const data = JSON.parse(body);
-  console.log(data.title);
+function getMovieTitleById(movieId) {
+  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+  request(apiUrl, (error, response, body) => {
+      if (error) {
+      console.error('Error:', error);
+    
+    } else {
+      if (response.statusCode === 200) {
+        const movieData = JSON.parse(body);
+        console.log(`${movieData.title}`);
+      } else {
+        console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
+     }
+    }
+  });
 }
-});
+
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Please provide a movie ID');
+} else {
+  getMovieTitleById(movieId);
+}
