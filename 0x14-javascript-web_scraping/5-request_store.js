879ad4e35870d5ea9ce6af31f77b3b3e83608d66
ofentseloeto <ofentseloeto610@gmail.com>
2023-07-26 14:19:39 -0700

    Gettting the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,34 +1,27 @@
 #!/usr/bin/node
 // script that gets the contents of a webpage and stores it in a file
 
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
 
-function fetchPageContent (url, filePath) {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      if (response.statusCode === 200) {
-        fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-          if (writeError) {
-            console.error('Error writing to file:', writeError);
-          } else {
-            console.log(`Webpage content saved to ${filePath}`);
-          }
-        });
-      } else {
-        console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-      }
-    }
-  });
+if (process.argv.length !== 4) {
+  console.error('Usage: node request_store.js <URL> <file_path>');
+  process.exit(1);
 }
 
 const url = process.argv[2];
-const filePath = process.argv[4];
+const filePath = process.argv[3];
 
-if (!url || !filePath) {
-  console.error('Provide URL and filePath');
-} else {
-  fetchPageContent(url, filePath);
-}
+request.get({ url, encoding: 'utf-8' }, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error.message);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+      if (writeError) {
+        console.error('Error:', writeError.message);
+      } else {
+        console.log(filePath);
+      }
+    });
+  }
+});
