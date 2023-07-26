b0256806bbb991a4b9a99026c4e01a3cb1d1cb9d
ofentse loeto <ofentseloeto610@gmail.com>
2023-07-26 17:49:45 +0000

    Fetching webpage content and store it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file
+
+const fs = require('fs');
+const request = require('request');
+
+function fetchPageContent(url, filePath) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+
+} else { 
+  
+  if (response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+  
+  if (writeError) {
+    console.error('Error writing to file:', writeError);
+} else {
+  console.log(`Webpage content saved to ${filePath}`);
+}
+});
+
+} else {
+  console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
+}
+}
+});
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Provide URL and filePath');
+
+} else {
+  fetchPageContent(url, filePath);
+}
