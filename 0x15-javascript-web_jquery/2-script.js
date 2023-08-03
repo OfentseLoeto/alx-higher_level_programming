f2c6b3fa1458b101e48f049b73e1fa6268b049e7
ofentseloeto <ofentseloeto610@gmail.com>
2023-08-02 20:59:37 -0700

    tast complete

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+const $ = window.$;
+$(document).ready(function () {
+  $('#red_hearder').click(function () {
+    $('header').css('color', '#FF0000');
+  });
+});
---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+const $ = window.$;
+$(document).ready(function () {
+  $('#red_reader').click(function () {
+    $('header').addClass('red');
+  });
+});
