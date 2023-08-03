935c2b9da45580126d0c8e2f7f9c20b13c828b82
ofentseloeto <ofentseloeto610@gmail.com>
2023-08-02 21:05:35 -0700

    tast complete

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,7 +1,6 @@
 const $ = window.$;
 $(document).ready(function () {
   $('#add_item').click(function () {
-    const newItem = $('<li>Item</li>');
     $('ul.my_list').append('newItem');
   });
 });
